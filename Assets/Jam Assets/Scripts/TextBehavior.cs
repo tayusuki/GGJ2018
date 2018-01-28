52e2656c8c730c28d2330d4805b2cd0fc5825219
tayusuki <aestrada2@mail.pima.edu>
2018-01-28 06:14:21 -0700

    Script/Scene Changes
    - Fleshed out the UI for 7 day
    - Skeletoned TextBehavior script

--- Assets/Jam Assets/Scripts/TextBehavior.cs
+++ Assets/Jam Assets/Scripts/TextBehavior.cs
@@ -6,11 +6,16 @@ using UnityEngine.UI;
 public class TextBehavior : MonoBehaviour {
 
     string[] headlines = new string[5];
+    public Image[] icons = new Image[7];
+    public Text[] DOW = new Text[7];
+    public Text[] description = new Text[7];
+    public Text[] high = new Text[7];
+    public Text[] low = new Text[7];
 
-    public Text textbox;
+    public Text headline;
 
 	void Start () {
-        textbox.text = headlines[Random.Range(0, headlines.Length - 1)];
+        headline.text = headlines[Random.Range(0, headlines.Length - 1)];
 	}
 	
 	void Update () {
