0b8d8fb00563faf14d443a91e3afefd5bf901bbd
tayusuki <aestrada2@mail.pima.edu>
2018-01-28 02:43:00 -0700

    Added Assets/Menu Drafts
    - Added 2 full body MC images
    - Added raincloud images
    - Generated drafted GAME OVER screen
    - Generated shell menus
    - Exit script written
    - Imported fonts

---
+++ Assets/Jam Assets/Scripts/TextBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBehavior : MonoBehaviour {
+
+    string[] headlines = new string[5];
+
+    public Text textbox;
+
+	void Start () {
+        textbox.text = headlines[Random.Range(0, headlines.Length - 1)];
+	}
+	
+	void Update () {
+		
+	}
+}
