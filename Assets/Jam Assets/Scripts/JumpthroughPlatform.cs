374eb797518dd0a1b03887a678ac24294a2035bb
DigiGee <byongbanthom@yahoo.com>
2018-01-28 05:04:39 -0700

    Jump platforming
    heyyyy yooo listen what I sayyyy yooooo

---
+++ Assets/Jam Assets/Scripts/JumpthroughPlatform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpthroughPlatform : MonoBehaviour {
+	private PlayerController pc;
+	private Collider col;
+	private bool fallingthru = false;
+
+	void Awake() {
+		pc = FindObjectOfType<PlayerController> ();	
+		col = GetComponent<Collider> ();
+		col.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (pc.transform.position.y - 1.7f > transform.position.y && !fallingthru) {
+			col.enabled = true;
+		} else if (col.enabled == true){
+			col.enabled = false;
+		}
+	}
+
+	public void DisablePlat(){
+		pc.transform.position += Vector3.down * 0.75f;
+		col.enabled = false;
+	}
+}
